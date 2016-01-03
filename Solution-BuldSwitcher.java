d8aefedc60e7ecc9e5545a85d6bd9f2dce2250ea
Pawan <Pawan.G.Mahalle@gmail.com>
2016-01-03 16:03:16 -0700

    Added solution to problem https://leetcode.com/problems/bulb-switcher/

---
+++ Solution-BuldSwitcher.java
@@ -0,0 +1,12 @@
+/**
+ * Problem Statement: https://leetcode.com/problems/bulb-switcher/
+ * 
+ * @author PawanMahalle
+ *
+ */
+
+public class Solution {
+    public int bulbSwitch(int n) {
+        return (int)Math.sqrt(n);   
+    }
+}
